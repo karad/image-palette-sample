0806fffca400e0482e4be9d078b1d7b74424a760
karad <kazuhiroh+karad@gmail.com>
2019-06-03 23:41:06 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const NodeVibrant = require('node-vibrant');
+const rgbHex = require('rgb-hex');
+
+/**
+ * Pick up palette from image
+ * @param {string} img_path 
+ */
+const pickPalette = async (img_path) => {
+    const palettes = await NodeVibrant.from(img_path, {
+        // options here
+    }).getPalette()
+
+    Object.getOwnPropertyNames(palettes).map((palette) => {
+        console.log(`${palette} : ${rgbToHex(palettes[palette]._rgb)}`)
+    })
+}
+
+/**
+ * rgb to hex
+ * @param {string} rgb_arr 
+ */
+const rgbToHex = (rgb_arr) => {
+    const [r, g, b] = rgb_arr;
+    return '#' + rgbHex(r, g, b);
+}
+
+const main = () => {
+    // Read args
+    const [node, js, ...args] = process.argv
+    pickPalette(args[0])
+}
+
+main(args)
+
